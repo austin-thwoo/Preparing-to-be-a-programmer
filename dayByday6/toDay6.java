84759b8ed3744d83bf8332693d31afa6054690e2
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-15 21:55:41 +0900

    codeup 1013 정수2개 입력받아 그대로 출력하기

---
+++ dayByday6/toDay6.java
@@ -0,0 +1,16 @@
+package Main.dayByday6;
+
+import java.util.Scanner;
+
+public class toDay6 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int A;
+	int B;
+	
+	A = sc.nextInt();
+	B = sc.nextInt();
+	System.out.printf("%d %d",A,B);
+					
+}
+}
