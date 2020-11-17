d75d4679a42b7325c4dc27c9285b0487e2b6d9b9
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-17 23:51:23 +0900

    코드업 1014~1019

---
+++ toDay1.java
@@ -0,0 +1,19 @@
+package Main.dayByday1;
+
+import java.util.Scanner;
+
+public class toDay1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String X=sc.next();
+		String Y=sc.next();
+		
+		System.out.printf("%s %s" ,Y, X);
+
+		
+		
+	}
+	
+		
+	
+}
