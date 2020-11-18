d7f995bd86a2f5d9d4416afe4478021e529aa18a
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-18 23:55:51 +0900

    201118코드업 1024 : 단어 1개 입력받아 나누어 출력하기
    단어를 1개 입력받는다.
    
    입력받은 단어(영어)의 각 문자를
    
    한줄에 한 문자씩 분리해 출력한다.
    
    
    참고
    C언어에서는 문장을 저장하기 위해 특별한 방법을 사용하지 않고,
    배열이라는 연속된 공간에 문자들을 연속으로 한 개씩 저장하는 방법을 사용한다.
    단, 문자열(문자가 연속적으로 연결된 줄이라는 의미. 즉 문장)의 마지막임을 나타내기 위해
    문자열의 마지막에 널(NULL) 문자라는 특별한 의미를 가지는 문자를 삽입해 문장임을 나타낸다.
    
    널(NULL)은 영어 단어 자체의 의미로는, 아무런 가치가 없는 또는 아무것도 없는, 사용되지 않은 등의 의미를 가지는데,
    
    문자로는 '\0', 아스키문자의 정수값은 0을 의미하며 NULL 로도 사용할 수 있다.
    
    단어나 문장을 scanf("%s", ...); 로 입력받게 되면, 그 마지막에 널문자가 자동으로 입력되는 것이다.
    
    그러므로, 본 문제는 그렇게 저장되어있는 영문자들을 한 개씩 순차적으로 검사해서,
    
    문장의 마지막을 나타내는 널문자가 나올 때까지 모양을 만들어 출력하면 되는 것이다.
    
    나중에 조건실행문, 반복문, 배열에 대해서 배우면 명확히 이해할 수 있게 된다.
    
    printf("%s", ...); 는 결국, 저장되어있는 문자의 마지막까지, 즉 널문자가 나올 때까지 형식에 맞추어 출력해 주라는 의미를 가진다.
    
    
    예시
    char d[30]; //최대 30문자를 저장할 수 있는 저장 공간 준비. 마지막에 널문자 고려해야함
    scanf("%s", d); //그 공간에 키보드로 입력된 내용을 저장. 단 공백이 있으면 거기까지만 입력됨.
    for(i=0; d[i]!='\0'; i++) //저장된 내용을 하나하나씩 검사해서 널문자가 아닐 동안 아래 실행
    {
       printf("\'%c\'", d[i]);

--- toDay5.java
+++ toDay5.java
@@ -5,23 +5,10 @@ import java.util.Scanner;
 public class toDay5 {
 	public static void main(String[] args) {
 
-		Scanner scanner= new Scanner(System.in);
-		
-		String X = scanner.next();
-		
-		String day[] = X.split("\\.");
-		
-		
-				
-		int to=Integer.parseInt(day[0]);
-		int too=Integer.parseInt(day[1]);
-		int tooo=Integer.parseInt(day[2]);
-		System.out.printf("%04d.%02d.%02d", to,too,tooo);
-		/*
-		System.out.printf
-		("%04d" "%2d" "%02d", X);
-		*/
-		
+		Scanner sc= new Scanner(System.in);
+		String X= sc.next();
+		for (int i=0; i<X.length(); i++ ) {
+			System.out.printf("\'%s\'\n",X.charAt(i));
 		
 	}
 }
