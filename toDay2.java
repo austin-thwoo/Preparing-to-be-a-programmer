fdc4e83ea91cad3c9a23b922e457762544204004
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-18 23:52:26 +0900

    201118 코드업 1021 : 단어 1개 입력받아 그대로 출력하기
    1개의 단어를 입력받아 그대로 출력해보자.

--- toDay2.java
+++ toDay2.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class toDay2 {
 	public static void main(String[] args) {
-		Scanner scanner= new Scanner(System.in);
-		double S= scanner.nextDouble();
-		System.out.printf("%.2f",S );
+		Scanner sc = new Scanner(System.in);
+		String str=sc.next();
+		System.out.println(str);
 		
 		
 	}
