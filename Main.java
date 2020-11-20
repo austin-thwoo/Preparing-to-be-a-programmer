9eceddae25dbba06e446fe2f42bdf75f19527965
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-20 17:28:41 +0900

    201120코드업 1028 : 년월일 입력 받아 형식 바꿔 출력하기
    정수 1개를 입력받아 그대로 출력해보자.
    (단, 입력되는 정수의 범위는 0 ~ 4,294,967,295 이다.)
    
    참고
    -2147483648 ~ +2147483647 범위의 정수를 저장하고 처리하기 위해서는
    int 형으로 변수를 선언하면 된다.(int 로 선언하고 %d로 받고 출력)
    
    하지만 이 범위를 넘어가는 정수를 저장하기 위해서는
    보다 큰 범위를 저장할 수 있는 다른 데이터형을 사용해야 정상적으로 저장시킬 수 있다.
    
    unsigned int 데이터형을 사용하면 0 ~ 4294967295 범위의 정수를 저장할 수 있다.
    
    
    예시
    unsigned int n;
    scanf("%u", &n);
    printf("%u", n);

--- Main.java
+++ Main.java
@@ -4,15 +4,10 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
-String str=sc.next();
-str=str.replace(".", "-");
-String[] arr = str.split("-");
-		String A=arr[2];
-		String B=arr[1];
-		String C=arr[0];
+
+long str = sc.nextLong();
 		
-		System.out.printf("%s-%s-%s",A,B,C);
-		
+System.out.print(str);
 }
 }
 
