541768b46ff9555e320b9c6697d43f69c987d13e
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-20 17:26:48 +0900

    201120코드업 1027 : 년월일 입력 받아 형식 바꿔 출력하기
    년월일을 출력하는 방법은 나라마다, 형식마다 조금씩 다르다.
    
    년월일(yyyy.mm.dd)를 입력받아,
    
    일월년(dd-mm-yyyy)로 출력해보자.
    
    (단, 한 자리 일/월은 0을 붙여 두자리로, 년도도 0을 붙여 네자리로 출력한다.) 
    
    
    참고
    출력하는 자리수를 지정하기 위해 %4d와 같은 형식을 사용할 수 있는데,
    빈칸을 0으로 출력하기 위해서는 %04d와 같은 형식을 사용하면 된다.
    
    예시
    printf("%02d-%02d-%04d", d, m, y);

--- Main.java
+++ Main.java
@@ -1,33 +1,18 @@
 package main.main;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
-
 import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 String str=sc.next();
-String[] arr = str.split(":");
-		System.out.println(Integer.parseInt(arr[1]));
+str=str.replace(".", "-");
+String[] arr = str.split("-");
+		String A=arr[2];
+		String B=arr[1];
+		String C=arr[0];
+		
+		System.out.printf("%s-%s-%s",A,B,C);
+		
 }
 }
 
-
-
-
-
-//1 1 1 1 1 1 1 1 1 1
-//1 0 0 1 0 0 0 0 0 1
-//1 0 0 1 1 1 0 0 0 1
-//1 0 0 0 0 0 0 0 0 1
-//1 0 1 1 0 0 0 1 0 1
-//1 0 0 0 0 1 0 1 1 1
-//1 0 0 0 0 1 0 1 0 1
-//1 0 0 0 0 1 0 0 0 1
-//1 0 0 0 0 0 0 0 0 1
-//1 1 1 1 1 1 1 1 1 1
