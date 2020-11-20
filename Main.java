5dc3c84299d7872c0c129792ef0924c09f01b939
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-20 17:23:25 +0900

    201120코드업 1025 : 단어 1개 입력받아 나누어 출력하기
    다섯 자리의 정수 1개를 입력받아 각 자리별로 나누어 출력한다.
    
    참고
    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
    를 사용하면 1개의 숫자로 각각 구분되어 저장된다.
    
    예시
    읽어들인 값을 적당히 바꿔 출력하기 위해서
    
    printf("[%d]", a*10000);
    
    과 같은 방법도 가능하다.

--- Main.java
+++ Main.java
@@ -1,24 +1,36 @@
-package Main.main;
+package main.main;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
+
    public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   String str=sc.next();
+	   String[] arr = str.split("");
+	   		
 
-	   
-	 
-	   System.out.print("\"!@#$%^&*()\"");
-      
+	   	System.out.println("[" + Integer.parseInt(arr[0])*10000+"]");
+	   	System.out.println("[" + Integer.parseInt(arr[1])*1000+"]");
+	   	System.out.println("[" + Integer.parseInt(arr[2])*100+"]");
+	   	System.out.println("[" + Integer.parseInt(arr[3])*10+"]");
+	   	System.out.println("[" + Integer.parseInt(arr[4])*1+"]");
       }
-      
-}  
-//      
-////       5
-//      1 1
-//      2 2
-//      3 3
-//      4 4
-//      5 5
-      
-      
-      
-   
+   }
+}
 
+
+//1 1 1 1 1 1 1 1 1 1
+//1 0 0 1 0 0 0 0 0 1
+//1 0 0 1 1 1 0 0 0 1
+//1 0 0 0 0 0 0 0 0 1
+//1 0 1 1 0 0 0 1 0 1
+//1 0 0 0 0 1 0 1 1 1
+//1 0 0 0 0 1 0 1 0 1
+//1 0 0 0 0 1 0 0 0 1
+//1 0 0 0 0 0 0 0 0 1
+//1 1 1 1 1 1 1 1 1 1
