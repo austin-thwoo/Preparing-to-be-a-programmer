df91ab73d04ddbe1135da9c787667e509a82cc27
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-20 17:24:37 +0900

    201120코드업 1026 :  시분초 입력받아 분만 출력하기
    입력되는 시:분:초 에서 분만 출력해보자.
    
    참고
    int h, m, s;
    scanf("%d:%d:%d", &h, &m, &s);
    
    를 실행하면 콜론을 사이에 둔 형식으로 입력되어, h, m, s에 각각 정수값만 저장된다.

--- Main.java
+++ Main.java
@@ -6,22 +6,19 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Main {
-
-   public static void main(String[] args) {
-	   Scanner sc = new Scanner(System.in);
-	   String str=sc.next();
-	   String[] arr = str.split("");
-	   		
-
-	   	System.out.println("[" + Integer.parseInt(arr[0])*10000+"]");
-	   	System.out.println("[" + Integer.parseInt(arr[1])*1000+"]");
-	   	System.out.println("[" + Integer.parseInt(arr[2])*100+"]");
-	   	System.out.println("[" + Integer.parseInt(arr[3])*10+"]");
-	   	System.out.println("[" + Integer.parseInt(arr[4])*1+"]");
-      }
-   }
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String str=sc.next();
+String[] arr = str.split(":");
+		System.out.println(Integer.parseInt(arr[1]));
 }
+}
+
+
+
 
 
 //1 1 1 1 1 1 1 1 1 1
