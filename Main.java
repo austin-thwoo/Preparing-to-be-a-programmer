9645ea8fbe3d77c73b86a0c8bbd628706857defa
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-24 10:08:05 +0900

    하샤드 수 출력하기
    입력받은 값이 하사드면 true
    하사드가 아닐경우 false 
    를 과정과 함께 출력하는 프로그램

--- Main.java
+++ Main.java
@@ -1,105 +1,30 @@
-package Main.main;
+package codup;
 
 import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) {
-//	   0번
-	  Scanner sc = new Scanner(System.in);
-	  int[][] pan= new int [20][20];
-	  int il = 1;
-	  int young=0;
-	  int x=sc.nextInt();
-	  int y=sc.nextInt();
-
-
-//	  1번
-	  for (int i = 0; i <= 18; i++) {
-		for (int j = 0; j <= 18; j++) {
-			pan[i][j]=sc.nextInt();
-		}
-	}
-	 
-	 int A=sc.nextInt();
-//	 가로줄바꾸기
-	 for (int i = 1; i <= A; i++) {
-			for (int j = 1; j < 18; j++) {
-				
-				if (pan[x][j]==1) {
-					pan[x][j]=0;
-				}else {
-					pan[x][j]=1;
-				}for (int j2 = 0; j2 < pan.length; j2++) {
-					if (pan[x][j2]==1) {
-						pan[x][j2]=0;
-					}else {
-						pan[x][j2]=1;
-					}
-				}
-			}
-		}
-//	 세로줄바꾸기
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	
+	boolean result = true;
+	
 
 	
-//	  2번
-	  for (int i = 0; i < pan.length-1; i++) {
-		for (int j = 0; j < pan.length-1; j++) {
-			System.out.print(pan[j][i]+" ");
-		}System.out.println();
-	}
-//	for (int i = 0; i < ber; i++) {
-//		int A=sc.nextInt();
-//		int B=sc.nextInt();
-//		if (pan [B][A]==0) {
-//			pan  [B][A]=il;
-//		}else {pan  [B][A]=young;}
-//	}
+	while(true) {
+	int x =sc.nextInt();
+	int A=x/10;
+	int B=x-(A*10);
 	
-   }
-   
+	
+	if(x%(A+B)==0) {result=true;
+	System.out.println(A+"+"+B+"="+(A+B)+"-->"+x+"/"+(A+B)+"=="+"0"+"--->"+result);
+	}
+	
+	else if(x%(A+B)!=0) {result=false;
+	System.out.println(A+"+"+B+"="+(A+B)+"-->"+x+"/"+(A+B)+"!="+"0"+"--->"+result);
+	}
+	}
+	
+	
 }
-	
-	
-			
-		
-	
-	
-		 	 
-//	 System.out.println();
-//	for (int i = 0; i < pan.length/2+1; i++) {
-//		for (int j = 0; j < pan.length/2+1; j++) {
-//			
-//			System.out.print(pan[(i)][(j)]+ " ");
-//		}System.out.println();
-//	}
-	 
-	  
-  
-/*    
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0
-2
-10 10
-12 12
-      */
-
-      
-   
-
+}
