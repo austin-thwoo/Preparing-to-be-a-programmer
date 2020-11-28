bf7ab2133e1cc44deac884c2c427bc756465362a
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-28 23:21:08 +0900

    입력한 숫자의 자릿수 별로 구분하여 출력하기
    ex)75854
     =>
    70000
    5000
    800
    50
    4

--- toDay4.java
+++ toDay4.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 
 public class toDay4 {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		String X = sc.nextLine();
-		String[] arr= X.split("\\."); 
-		System.out.printf("%s\n%s",arr[0], arr[1]);
-		
-		
+
+
+		String a = sc.next();
+		String[] arr = a.split("");
+		double b=arr.length;
+
+		for (int i = 0; i < arr.length; i++) {
+			double c=Math.pow(10, b-1);
+			long IN;   
+			IN=Integer.parseInt(arr[i]);
+			System.out.printf("%.0f",IN*c);
+			System.out.println();
+			b--;
+		}
 	}
 
+
+
+
+
 }
+
+
