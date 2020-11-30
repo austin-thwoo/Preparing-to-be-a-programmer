aac8d25c0ed5a196cb8e9b1c18eb7a03bc33cc3b
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-30 22:24:59 +0900

    [20201130] 코드 업 1029 : 실수 1개 입력받아 그대로 출력하기2

--- toDay5.java
+++ toDay5.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class toDay5 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		Scanner sc= new Scanner(System.in);
-		String X= sc.next();
-		for (int i=0; i<X.length(); i++ ) {
-			System.out.printf("\'%s\'\n",X.charAt(i));
-		
+		double str = sc.nextDouble();
+				
+		System.out.printf("%.11f",str);
 	}
 }
