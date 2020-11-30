7200e601806860eaa92a29d42adf4a961551c2d3
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-11-30 22:24:19 +0900

    [20201130] 코드 업 1028 : 정수 1개 입력받아 그대로 출력하기2
    정수 1개를 입력받아 그대로 출력해보자.
    (단, 입력되는 정수의 범위는 0 ~ 4,294,967,295 이다.)
    
    참고
    -2147483648 ~ +2147483647 범위의 정수를 저장하고 처리하기 위해서는
    int 형으로 변수를 선언하면 된다.(int 로 선언하고 %d로 받고 출력)
    
    하지만 이 범위를 넘어가는 정수를 저장하기 위해서는
    보다 큰 범위를 저장할 수 있는 다른 데이터형을 사용해야 정상적으로 저장시킬 수 있다.
    
    unsigned int 데이터형을 사용하면 0 ~ 4294967295 범위의 정수를 저장할 수 있다.
    
    
    예시
    unsigned int n;
    scanf("%u", &n);
    printf("%u", n);

--- toDay4.java
+++ toDay4.java
@@ -6,25 +6,14 @@ public class toDay4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-
-		String a = sc.next();
-		String[] arr = a.split("");
-		double b=arr.length;
-
-		for (int i = 0; i < arr.length; i++) {
-			double c=Math.pow(10, b-1);
-			long IN;   
-			IN=Integer.parseInt(arr[i]);
-			System.out.printf("%.0f",IN*c);
-			System.out.println();
-			b--;
+		long str = sc.nextLong();
+				
+		System.out.print(str);
 		}
 	}
 
 
 
 
-
-}
 
 
