adbf8d2b90bc329fcdc8056f565b58cd464bcff7
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-12-01 23:08:40 +0900

    [20201201] 코드 업 1031 : 10진 정수 1개 입력받아 8진수로 출력하기
    10진수를 입력받아 8진수(octal)로 출력해보자.
    
    참고
    %d(10진수 형태)로 입력받고,
    %o를 사용해 출력하면 8진수(octal)로 출력된다.

--- toDay2.java
+++ toDay2.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class toDay2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str=sc.next();
-		String[] arr = str.split(":");
-				System.out.println(Integer.parseInt(arr[1]));
-		
+
+		long str = sc.nextLong();
+				
+		System.out.printf("%o",str);
 	}
 }
