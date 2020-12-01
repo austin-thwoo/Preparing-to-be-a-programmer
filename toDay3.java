2b937d79c73e1117ae3e4a23aa1417cb901e9925
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-12-01 23:09:40 +0900

    [20201201] 코드 업 1032 : 10진 정수 입력받아 16진수로 출력하기1
    10진수를 입력받아 16진수(hexadecimal)로 출력해보자.
    
    참고
    %d(10진수 형태)로 입력받고
    %x로 출력하면 16진수(hexadecimal) 소문자로 출력된다.
    
    10진법은 한 자리에 10개(0 1 2 3 4 5 6 7 8 9)의 문자를 사용하고,
    16진법은 한 자리에 16개(0 1 2 3 4 5 6 7 8 9 a b c d e f)의 문자를 사용한다.
    16진수의 a는 10진수의 10, b는 11, c는 12 ... 와 같다.

--- toDay3.java
+++ toDay3.java
@@ -6,13 +6,9 @@ public class toDay3 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		String str=sc.next();
-		str=str.replace(".", "-");
-		String[] arr = str.split("-");
-				String A=arr[2];
-				String B=arr[1];
-				String C=arr[0];
+
+		long str = sc.nextLong();
 				
-				System.out.printf("%s-%s-%s",A,B,C);
+		System.out.printf("%x",str);
 	}
 }
