b243faeba6813bbe219bd077592199bf3d460a22
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-12-03 22:00:01 +0900

    [20201203] 코드 업 1038 : 정수 2개 입력받아 합 출력하기1
    정수 2개를 입력받아 합을 출력하는 프로그램을 작성해보자.
    (단, 입력되는 정수는 -1073741824 ~ 1073741824 이다.)

--- Main.java
+++ Main.java
@@ -1,30 +1,14 @@
-package codup;
+package Main.Main;
 
 import java.util.Scanner;
 
 public class Main {
 public static void main(String[] args) {
-	Scanner sc= new Scanner(System.in);
-	
-	boolean result = true;
-	
+	Scanner sc = new Scanner(System.in);
 
-	
-	while(true) {
-	int x =sc.nextInt();
-	int A=x/10;
-	int B=x-(A*10);
-	
-	
-	if(x%(A+B)==0) {result=true;
-	System.out.println(A+"+"+B+"="+(A+B)+"-->"+x+"/"+(A+B)+"=="+"0"+"--->"+result);
-	}
-	
-	else if(x%(A+B)!=0) {result=false;
-	System.out.println(A+"+"+B+"="+(A+B)+"-->"+x+"/"+(A+B)+"!="+"0"+"--->"+result);
-	}
-	}
-	
-	
+	long str = sc.nextLong();
+	long str2 = sc.nextLong();		
+
+	System.out.println(str + str2);
 }
 }
