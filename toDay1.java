591a880f46970f8df0d7d7dc359b8bf813c6d507
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-12-21 22:23:16 +0900

    [20201221] 코드 업 1045 : 정수 2개 입력받아 합 출력하기2
    정수 2개(a, b)를 입력받아 합, 차, 곱, 몫, 나머지, 나눈 값을 자동으로 계산해보자.
    단 0 <= a, b <= 2147483647, b는 0이 아니다.

--- toDay1.java
+++ toDay1.java
@@ -1,18 +1,24 @@
-package Main.dayByday1;
+package Main.dayByDay1;
 
 import java.util.Scanner;
 
 public class toDay1 {
 	public static void main(String[] args) {
-		  Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		   long str = sc.nextLong();
-		   long str2 = sc.nextLong();		
+		int str = sc.nextInt();
+		int str2 = sc.nextInt();
 
-		   System.out.println(str + str2);
-		
+
+
+			System.out.println(str + str2);
+			System.out.println(str - str2);
+			System.out.println(str * str2);
+			System.out.println(str / str2);
+			System.out.println(str % str2);
+			float A=str;
+			float B=str2;
+			System.out.printf("%.2f", A/B);
+	
 	}
-	
-		
-	
-}
+	}
